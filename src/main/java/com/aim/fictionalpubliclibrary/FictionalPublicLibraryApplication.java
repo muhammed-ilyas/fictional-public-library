b7a29d2a9616bb4e1fce5e01af5993fd8ee92b14
SMI240 <shaik.ilyas@maersk.com>
2025-09-06 19:43:19 +0530

    Initial commit

---
+++ src/main/java/com/aim/fictionalpubliclibrary/FictionalPublicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.aim.fictionalpubliclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionalPublicLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FictionalPublicLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aim/fictionalpubliclibrary/FictionalPublicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aim.fictionalpubliclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictionalPublicLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
